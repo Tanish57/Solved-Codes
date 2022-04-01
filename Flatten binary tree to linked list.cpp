7780b8999e24a6f268397be61e9b8a6937ce7178
Tanish57 <68190217+Tanish57@users.noreply.github.com>
2022-04-01 08:08:23 +0530

    Create Flatten binary tree to linked list.cpp

---
+++ Flatten binary tree to linked list.cpp
@@ -0,0 +1,15 @@
+while (curr != NULL)
+{
+
+    if (curr->left)
+    {
+        Node *pred = curr->left;
+        while (pred->right)
+            pred = pred->right;
+
+        pred->right = curr->right;
+        curr->right = curr->left;
+        curr->left = NULL;
+    }
+    curr = curr->right;
+}
