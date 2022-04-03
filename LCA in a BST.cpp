5a811b840401d0ac5cf5ff171e0aa2cecbc143ce
Tanish57 <68190217+Tanish57@users.noreply.github.com>
2022-04-03 10:28:00 +0530

    Optimized Code using iterative approach
    Optimized Code using iterative approach to take constant space to run i.e O(1)

--- LCA in a BST.cpp
+++ LCA in a BST.cpp
@@ -1,15 +1,31 @@
+// Node* LCAinaBST(Node* root, Node* P, Node* Q)
+// {
+// 	// base case
+//     if(root == NULL){
+//         return NULL;
+//     }
+    
+//     if(root->data < P->data && root->data < Q->data)
+//         return LCAinaBST(root->right, P, Q);
+    
+//     if(root->data > P->data && root->data > Q->data)
+//         return LCAinaBST(root->left, P, Q);
+    
+//     return root;
+// }
+
+
+// optimized approach with SC -> O(1):
 Node* LCAinaBST(Node* root, Node* P, Node* Q)
 {
-	// base case
-    if(root == NULL){
-        return NULL;
+    while(root != NULL){
+        if(root->data < P->data && root->data < Q->data)
+            root = root->right;
+        
+        else if(root->data > P->data && root->data > Q->data)
+            root = root->left;
+        else{
+            return root;
+        }
     }
-    
-    if(root->data < P->data && root->data < Q->data)
-        return LCAinaBST(root->right, P, Q);
-    
-    if(root->data > P->data && root->data > Q->data)
-        return LCAinaBST(root->left, P, Q);
-    
-    return root;
 }
