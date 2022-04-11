8735a0e9e82764a7e61c9e073c40c8ed1d732a23
Tanish57 <68190217+Tanish57@users.noreply.github.com>
2022-04-11 22:37:31 +0530

    Create kth smallest element.cpp

---
+++ kth smallest element.cpp
@@ -0,0 +1,24 @@
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    priority_queue<int> pq;
+
+    // step 1:
+    for (int i = 0; i < k; i++)
+    {
+        pq.push(arr[i]);
+    }
+
+    // step 2:
+    for (int i = k; i <= r; i++)
+    {
+        if (arr[i] < pq.top())
+        {
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+
+    // step 3: ans = pq.top();
+    int ans = pq.top();
+    return pq.top();
+}
